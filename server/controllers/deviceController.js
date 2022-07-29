135b04fa508b6849f3b3d0a949e492cc404670f2
Rasa <abdurasul2004.02@gmail.com>
2022-07-30 04:32:31 +0500

    added routers

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,13 @@
+class DeviceController {
+    async create(req, rea){
+        
+    }
+    async getAll(req, rea){
+
+    }
+    async getOne(req, rea){
+
+    }
+}
+
+module.exports = new DeviceController()
