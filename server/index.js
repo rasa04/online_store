9b8fec4d7b968c6d68b531ae97257575759d9209
Rasa <abdurasul2004.02@gmail.com>
2022-07-30 21:15:13 +0500

    crud implemented

--- server/index.js
+++ server/index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
@@ -11,7 +15,12 @@ const app = express()
 //requests
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static'))) //files from static package must share as static, that because we can recieve them bby request
+app.use(fileUpload({}))
 app.use('/api', router)
+
+//errors handler calls last
+app.use(errorHandler)
 
 const start = async () => {
     try {
