ef87eaa5d058bc1459dca5ba7b7e7c1afd977761
Rasa <abdurasul2004.02@gmail.com>
2022-07-31 00:59:28 +0500

    get devices and setted pagination

--- server/models/models.js
+++ server/models/models.js
@@ -21,8 +21,8 @@ const BasketDevice = sequelize.define('basket_device', {
 const Device = sequelize.define('device', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
-    price: {type: DataTypes.STRING, allowNull: false},
-    rating: {type: DataTypes.STRING, unique: true, defaultValue: 0},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    rating: {type: DataTypes.INTEGER, defaultValue: 0},
     img: {type: DataTypes.STRING, allowNull: false},
 })
 
@@ -72,7 +72,7 @@ Rating.belongsTo(Device)
 Device.hasMany(BasketDevice)
 BasketDevice.belongsTo(Device)
 
-Device.hasMany(DeviceInfo)
+Device.hasMany(DeviceInfo, {as: 'info'})
 DeviceInfo.belongsTo(Device)
 
 Type.belongsToMany(Brand, {through: TypeBrand})
